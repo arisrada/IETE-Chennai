538f78642211c49748945ef9da255d5466d1a7ab
arisrada <arisradad2003@gmail.com>
2023-08-29 10:57:44 +0530

    updated about page

--- js/main.js
+++ js/main.js
@@ -25,3 +25,39 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+//number count
+function animateNumber(target, finalValue, duration) {
+  let current = 0;
+  let step = (finalValue - current) / (duration / 50);
+
+  function updateNumber() {
+    if (current <= finalValue) {
+      document.getElementById(target).textContent = Math.floor(current);
+      current += step;
+      setTimeout(updateNumber, 5);
+    } else {
+      document.getElementById(target).textContent = finalValue;
+    }
+  }
+
+  updateNumber();
+}
+
+function checkIfVisible(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateNumber("number1", 12060, 13000);
+      animateNumber("number2", 13592, 14000);
+      animateNumber("number3", 42, 500);
+      animateNumber("number4", 15, 300);
+      observer.unobserve(entry.target); 
+    }
+  });
+}
+
+const observer = new IntersectionObserver(checkIfVisible, { root: null, rootMargin: "0px", threshold: 0.5 });
+const countdownContainer = document.getElementById("countdownContainer");
+
+observer.observe(countdownContainer);
